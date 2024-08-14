9bf7be8282713650220680af5b7b7766993198be
tomsq <finixit@email.cz>
2024-08-14 10:09:00 +0200

    feat: load example template

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,30 @@
-import TopolEditor, { TopolPlugin, IReactTopolOptions} from "@topol.io/editor-react"
+import TopolEditor, {
+  TopolPlugin,
+  IReactTopolOptions,
+} from "@topol.io/editor-react";
 
 function App() {
-
   const topolOptions: IReactTopolOptions = {
     authorize: {
-      apiKey: 'YOUR_TOKEN',
-      userId: 1
+      apiKey: import.meta.env.VITE_TOPOL_EDITOR_API_KEY as string,
+      userId: 1,
     },
-    callbacks: {
+    callbacks: {},
+  };
 
-    }
+  function loadTemplate() {
+    fetch("https://tlapi.github.io/topol-editor/templates/1.json")
+      .then((response) => response.text())
+      .then((template) => {
+        TopolPlugin.load(template);
+      });
   }
 
-    return (
-      <div className="App">
-        <TopolEditor options={topolOptions}/>
-      </div>
-    )
-  
+  return (
+    <div className="App">
+      <TopolEditor options={topolOptions} onInit={loadTemplate} />
+    </div>
+  );
 }
 
-export default App
+export default App;
